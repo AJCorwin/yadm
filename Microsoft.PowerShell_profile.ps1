0d4eec33a974e9d4fe43fdc9d5963e3d0c6cf2c5
AJCorwin <AJCorwin@users.noreply.github.com>
2022-07-17 01:32:02 -0500

    Modified powershell theme script. Added oh my posh, and my own theme.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
 # scoop install starship
 # New-Item -Path $profile -Type File -Force
 # notepad $PROFILE
-Invoke-Expression (&starship init powershell)
+# Invoke-Expression (&starship init powershell)
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\.mytheme.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config ~/.FirewallTheme.omp.json | Invoke-Expression
