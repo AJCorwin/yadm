f56476309acc7fd939d733ae11a361fd16ba284b
AJCorwin <AJCorwin@users.noreply.github.com>
2022-07-17 21:28:27 -0500

    Fixed issue with powershell profile. renamed winterm settings to the proper name.

--- .config/windows-terminal/Microsoft.PowerShell_profile.ps1
+++ .config/windows-terminal/Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,9 @@
 # notepad $PROFILE
 # Invoke-Expression (&starship init powershell)
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\.mytheme.omp.json" | Invoke-Expression
-Import-Module oh-my-posh
-& "$env:LOCALAPPDATA\Programs\oh-my-posh\bin\oh-my-posh.exe" ~/.FirewallTheme.omp.json | Invoke-Expression
+Write-Host -NoNewline "`n`e[1F"
+oh-my-posh init pwsh --config ~/.FirewallTheme.omp.json | Invoke-Expression
+
+
+# Alias
+Set-Alias grep findstr
--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-# scoop install starship
-# New-Item -Path $profile -Type File -Force
-# notepad $PROFILE
-# Invoke-Expression (&starship init powershell)
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\.mytheme.omp.json" | Invoke-Expression
-Import-Module oh-my-posh
-& "$env:LOCALAPPDATA\Programs\oh-my-posh\bin\oh-my-posh.exe" ~/.FirewallTheme.omp.json | Invoke-Expression
