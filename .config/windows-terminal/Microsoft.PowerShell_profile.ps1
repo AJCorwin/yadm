088accd796869fe466da34330f8cc180af34e7c0
AJCorwin <AJCorwin@users.noreply.github.com>
2022-07-17 21:52:16 -0500

    Added functions to powershell profile to change directory. Added import terminal icons

--- .config/windows-terminal/Microsoft.PowerShell_profile.ps1
+++ .config/windows-terminal/Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,12 @@
 # notepad $PROFILE
 # Invoke-Expression (&starship init powershell)
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\.mytheme.omp.json" | Invoke-Expression
+Import-Module Terminal-Icons
 Write-Host -NoNewline "`n`e[1F"
 oh-my-posh init pwsh --config ~/.FirewallTheme.omp.json | Invoke-Expression
 
 
 # Alias
 Set-Alias grep findstr
+function ghr { set-location "G:\Github-Repos" }
+function gcode { set-location "G:\Code" }
