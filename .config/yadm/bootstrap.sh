b9df80c698d38ae8344794d46dd086dcea7fe375
AJCorwin <AJCorwin@users.noreply.github.com>
2022-11-13 02:00:24 -0600

    Its late, last test.

--- .config/yadm/bootstrap.sh
+++ .config/yadm/bootstrap.sh
@@ -34,7 +34,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install node
-LV_BRANCH='release-1.2/neovim-0.8' /bin/bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh [--[yes-]install-dependencies ] | LV_BRANCH='release-1.2/neovim-0.8' bash
 
 yadm checkout "/home/$USER"
 #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
