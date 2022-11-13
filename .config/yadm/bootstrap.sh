af072cd8ef654d16182360841874052392b66d2f
AJCorwin <AJCorwin@users.noreply.github.com>
2022-11-12 22:35:28 -0600

    adding nvm changes

--- .config/yadm/bootstrap.sh
+++ .config/yadm/bootstrap.sh
@@ -28,7 +28,11 @@ cd "$HOME/playbooks"
 ansible-playbook --diff --extra-vars "@$CONFIG_DIR/values.yaml" "$DOTFILES_DIR/main.yaml" "$@" --ask-become-pass -vv
 cd $HOME
 yadm checkout "/home/$USER"
-zsh .zshrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install node
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
--- .profile
+++ .profile
@@ -30,6 +30,7 @@ if [ -f "$HOME/.cargo/bin/bat" ] ; then
     alias cat="bat"
 fi
 
+export NVM_DIR="$HOME/.nvm"
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
